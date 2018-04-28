6476a57a3ab88e5fbedbc66d7abff29db3bf2c69
Christina Koch <christinakconnect@gmail.com>
2018-04-27 20:34:31 -0500

    changing structure for make csv

--- scripts/make_csv.py
+++ scripts/make_csv.py
@@ -1,15 +1,19 @@
 import sys
+import os
 
 script = sys.argv[0]
 infile = sys.argv[1]
 
-outfile = infile[0:-4]+".csv"
+output_data_path = os.path.split(infile)[0] + '/../csv/'
+outfile_name = os.path.splitext(os.path.basename(infile))[0]+".csv"
+
+outfile = output_data_path + outfile_name
 
 input_file = open(infile,'r')
 output_file = open(outfile, 'w')
 
 for num,line in enumerate(input_file):
     output_file.write(line.strip().split(' ')[2] + '\r')
-    
+
 input_file.close()
 output_file.close()
